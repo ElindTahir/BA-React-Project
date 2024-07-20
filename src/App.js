df69d9ae3b8cc17d0c12a67b1f6a9f8abc188187
ElindTahir <if21b033@technikum-wien.at>
2024-07-20 14:58:47 +0200

    React refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import LandingPage from './pages/LandingPage/LandingPage';
-import DatabasePage from './pages/DatabasePage/DatabasePage';
-import FetchAPIPage from './pages/FetchApiPage/FetchApiPage';
-import ChartPage from './pages/ChartPage/ChartPage';
+import Navbar from './components/Navbar/Navbar.tsx';
+import LandingPage from './pages/LandingPage/LandingPage.tsx';
+import DatabasePage from './pages/DatabasePage/DatabasePage.tsx';
+import FetchAPIPage from './pages/FetchApiPage/FetchApiPage.tsx';
+import ChartPage from './pages/ChartPage/ChartPage.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
